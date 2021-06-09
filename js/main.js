ac94ac9473947e897522832a9fc06802fadc22a2
Tyrone Dale Power <46249992+TyDPower@users.noreply.github.com>
2021-06-09 21:48:12 +0100

    User location
    Load map and detect the location of the user, with pin and radius.

---
+++ js/main.js
@@ -0,0 +1,30 @@
+var map = L.map('map').fitWorld();
+
+const worldMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+    tileSize: 512,
+    zoomOffset: -1,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+worldMap.addTo(map);
+
+map.locate({setView: true, maxZoom: 16});
+
+function onLocationFound(e) {
+    var radius = e.accuracy;
+
+    L.marker(e.latlng).addTo(map)
+        .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+    L.circle(e.latlng, radius).addTo(map);
+}
+
+map.on('locationfound', onLocationFound);
+
+function onLocationError(e) {
+    alert(e.message);
+}
+
+map.on('locationerror', onLocationError);
+
