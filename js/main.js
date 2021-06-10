e3f4fffd3d18e8a17f8df721afa1d12d77c301c1
Tyrone Dale Power <46249992+TyDPower@users.noreply.github.com>
2021-06-10 22:38:50 +0100

    change map tile layer

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
 var map = L.map('map').fitWorld();
 
-const worldMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	maxZoom: 19,
-    tileSize: 512,
-    zoomOffset: -1,
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+const worldMap = L.tileLayer('https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png', {
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
 });
 
 worldMap.addTo(map);
