be50c464702d4da549351079589ff9ce4f9cce9d
Tyrone Dale Power <46249992+TyDPower@users.noreply.github.com>
2021-06-11 16:36:19 +0100

    Test code for getBounds method
    Test code to get the approx center of selected country.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,14 @@
 //Initial World map tiles
 var map = L.map('map').fitWorld();
-
 const worldMap = L.tileLayer('https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png', {
 	maxZoom: 20,
 	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
 });
-
 worldMap.addTo(map);
 
 //Grab user location with marker and country select
 map.locate({setView: true, maxZoom: 16});
-
-function onLocationFound(e) {
+const onLocationFound = (e) => {
     var radius = e.accuracy;
 
     L.marker(e.latlng).addTo(map)
@@ -53,28 +50,44 @@ function onLocationFound(e) {
     }
     
 }
-
 map.on('locationfound', onLocationFound);
-
-function onLocationError(e) {
+const onLocationError = (e) => {
     alert(e.message);
 }
-
 map.on('locationerror', onLocationError);
 
 //Select new country with html drop bow menu
 $("#countryList").change(()=> {
     let countryCode = $("#countryList").val();
+    let countryBorder;
 
     $.getJSON("./common/countries.geo.json", (data)=> {
         for (let i=0; i<data.features.length; i++) {
             if (data.features[i].properties.ISO_A3 == countryCode) {
-                let countryBorder = data.features[i];
+                countryBorder = data.features[i];
                 $(".leaflet-interactive")[1].remove();
                 L.geoJSON(countryBorder).addTo(map);
+                console.log(countryBorder.geometry.coordinates.length)
+
+                for (let i=0; i<countryBorder.geometry.coordinates.length; i++) {
+                    let coords = [countryBorder.geometry.coordinates[i]]
+                    //console.log(coords)
+                    let bounds = L.bounds(coords);
+                    console.log(bounds.max.x + " " + bounds.max.y)
+                }
+                //map.panTo([-28.265682390146466, 23.9501953125]);
+                //let bounds = L.bounds([1, 3], [4, 5], [9, 3], [1, 3], [1, 3]);
+                //console.log(bounds);
                 break;
             }
         }
+
+        /*for (let i=0; i<data.features.length; i++) {
+            for (let f=0; f<data.features[i].geometry.coordinates.length; f++) {
+                console.log(i + " " + data.features[i].geometry.coordinates[f]);
+            }
+        }*/
+
     })
 
 })
