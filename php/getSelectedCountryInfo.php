9461ad66d20288e58e6a81de1ded74c7d41e16bc
Tyrone Dale Power <46249992+TyDPower@users.noreply.github.com>
2021-06-14 21:21:06 +0100

    Update getSelectedCountryInfo.php
    PHP API call using Curl to OpenCage API to get place infomation

---
+++ php/getSelectedCountryInfo.php
@@ -0,0 +1,30 @@
+<?php
+
+    ini_set("display_error", "On");
+    error_reporting(E_ALL);
+
+    $startTime = microtime(true);
+
+    $url="https://api.opencagedata.com/geocode/v1/json?q=" . $_REQUEST["placename"] . "&key=efbd5efcc6254578986dc2d63c0edb90"
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_URL, $url);
+
+    $result = curl_exec($ch);
+
+    curl_close($ch);
+
+    $decode = json_decode($result, true);
+
+    $output["status"]["code"] = "200";
+    $output["status"]["name"] = "ok";
+    $output["status"]["description"] = "success";
+    $output["data"] = $decode["geonames"];
+
+    header("Content-Type: application/json; charset=UTF-8");
+
+    echo json_encode($output);
+
+?>
