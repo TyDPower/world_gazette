fe492660f118592d8ac3823e204de89be152f06e
Tyrone Dale Power <46249992+TyDPower@users.noreply.github.com>
2021-06-15 22:25:45 +0100

    Update modal.js

--- common/modal.js
+++ common/modal.js
@@ -1,12 +1,22 @@
 export const countryInfo = (data) => {
-    $("#countryName").html(data);
-    $("#continent").html(data);
-    $("#flag").html(data);
-    $("#currencyName").html(data);
-    $("#currencySubunit").html(data);
-    $("#currencySymbol").html(data);
-    $("#driveSide").html(data);
-    $("#speedUnit").html(data);
-    $("#timezoneName").html(data);
-    $("#timezoneDst").html(data);
+
+    var checkDST = (dst) => {
+        if (dst === 1) {
+            return "Daylight Savings Time is currently active"
+        } else {
+            return "No Daylight Savings Time currently active"
+        }
+    }
+
+    $("#countryName").html(data.name);
+    $("#continent").html(data.continent);
+    $("#flag").html(data.flag);
+    $("#currencyName").html(data.currencyInfo.name);
+    $("#currencySubunit").html(data.currencyInfo.subunit);
+    $("#currencySymbol").html(data.currencyInfo.symbol);
+    $("#driveSide").html(data.roadInfo.driveSide);
+    $("#speedUnit").html(data.roadInfo.speedUnit);
+    $("#timezoneName").html(data.timezoneInfo.name);
+    $("#timezoneDst").html(checkDST(data.timezoneInfo.dst));
+    
 }
