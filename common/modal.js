24b0e053ed26f73d528484215d81c54dfd0bc7de
Tyrone Dale Power <46249992+TyDPower@users.noreply.github.com>
2021-06-15 21:54:26 +0100

    Update modal.js
    countryInfo function to update the dom

--- common/modal.js
+++ common/modal.js
@@ -1,3 +1,12 @@
-export const heading = (name) => {
-    $("#modalHeading").html(name);
+export const countryInfo = (data) => {
+    $("#countryName").html(data);
+    $("#continent").html(data);
+    $("#flag").html(data);
+    $("#currencyName").html(data);
+    $("#currencySubunit").html(data);
+    $("#currencySymbol").html(data);
+    $("#driveSide").html(data);
+    $("#speedUnit").html(data);
+    $("#timezoneName").html(data);
+    $("#timezoneDst").html(data);
 }
