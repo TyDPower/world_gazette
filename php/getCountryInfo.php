59bbc58bc74473e0d37ca3d5fc1df0581cbfeff2
Tyrone Dale Power <46249992+TyDPower@users.noreply.github.com>
2021-06-23 22:32:28 +0100

    Create getCountryInfo.php
    Changed from getPlaceInfo to getCountryInfo
    More semantic name

---
+++ php/getCountryInfo.php
@@ -0,0 +1,31 @@
+<?php
+
+    ini_set("display_error", "On");
+    error_reporting(E_ALL);
+
+    $startTime = microtime(true);
+    $apiKey = "efbd5efcc6254578986dc2d63c0edb90";
+
+    $url="https://api.opencagedata.com/geocode/v1/json?q=" . $_REQUEST["countryName"] . "&countrycode=" . $_REQUEST["isoCode"] . "&key=" . $apiKey;
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_URL, $url);
+
+    $result = curl_exec($ch);
+
+    curl_close($ch);
+
+    $decode = json_decode($result, true);
+
+    $output["status"]["code"] = "200";
+    $output["status"]["name"] = "ok";
+    $output["status"]["description"] = "success";
+    $output["data"] = json_decode($result, true);
+
+    header("Content-Type: application/json; charset=UTF-8");
+
+    echo json_encode($output);
+
+?>
