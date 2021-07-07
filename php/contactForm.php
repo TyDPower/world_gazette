5773c4d2672e89ffd15294539f1a51290714a41b
Tyrone Dale Power <46249992+TyDPower@users.noreply.github.com>
2021-07-08 00:06:20 +0100

    Update contactForm.php

---
+++ php/contactForm.php
@@ -0,0 +1,44 @@
+<?php
+
+    mail("tyrone.power@live.com", "Test: 07/07 21:48", "Test email");
+
+
+    /*$emailTo = "contact@typower.info";
+
+    if (empty($_POST["name"]) || empty($_POST["email"]) || empty($_POST["message"])) {
+        $errors .= "\n Error: all fields are required";
+    }
+
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    if (!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", $email)) {
+            $errors .= "\n Error: Invalid email address";
+    }
+
+    if (empty($errors)) {
+
+        $to = $emailTo;
+
+        $email_subject = "Contact form submission: $name";
+
+        $email_body = "You have received a new message. ".
+
+        " Here are the details:\n Name: $name \n ".
+
+        "Email: $email\n Message \n $message";
+
+        $headers = "From: $emailTo\n";
+
+        $headers .= "Reply-To: $email";
+
+        mail($to,$email_subject,$email_body,$headers);
+
+        //redirect to the 'thank you' page
+
+        header('Location: contact-form-thank-you.html');
+
+        }*/
+
+?>
