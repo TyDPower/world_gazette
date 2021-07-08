9e33f31a6f06b0726f9fa12f2bdecaecc2a8ed58
Tyrone Dale Power <46249992+TyDPower@users.noreply.github.com>
2021-07-08 22:52:46 +0100

    Update modal.js
    Changed the way country information is displayed in the modal

--- common/modal.js
+++ common/modal.js
@@ -1,13 +1,24 @@
 import * as utilities from "./utilities.js"
 
-export const countryInfo = (selectedCountry, userCountry) => {
+export const countryModal = (selectedCountry, userCountry) => {
     
     const displayLanguages = () => {
         $("#languages").html("");
         selectedCountry.social.languages.forEach(res=>{$("#languages").append(`${res.name}<br>`)});
     }
 
+    const displayTimezones = () => {
+        $("#timezone").html("");
+        for (let i=0; i<selectedCountry.timezones[0].length; i++) {
+            $("#timezone").append(`${selectedCountry.timezones[0][i]}<br>`)
+        }
+    }
+
     $("#countryName").html(selectedCountry.admin.name);
+    $("#countryRegion").html(` (${selectedCountry.admin.region},`);
+    $("#countrySubRegion").html(` ${selectedCountry.admin.subregion})`);
+
+    $("#capital").html(`Capital City: ${selectedCountry.admin.capital}`);
     $("#population").html("Population: " + selectedCountry.social.population);
     $("#languagesTitle").html(selectedCountry.admin.name + "'s languages:")
     $("#flag").attr("src", selectedCountry.flag.large);
@@ -19,16 +30,29 @@ export const countryInfo = (selectedCountry, userCountry) => {
 
     $("#foreignExchange").html(utilities.checkValidCurrency(userCountry.currency.symbol) + "1 = " + selectedCountry.currency.symbol + selectedCountry.currency.exchangeRate[1].toFixed(3));
 
-    $("#driveSide").html("Drive on the ?? side of the road.");
-    $("#speedUnit").html("Speed is mesured in ??");
+    $("#livingInfoComparison").html(`*This is a general comparison of living conditions in ${selectedCountry.admin.name} with ${userCountry.admin.name}`);
 
-    $("#trafficIndex").html(utilities.compareIndex("traffic", selectedCountry, userCountry));
-    $("#qualityOfLifeIndex").html(utilities.compareIndex("qualityOfLife", selectedCountry, userCountry));
-    $("#healthcareIndex").html(utilities.compareIndex("healthcare", selectedCountry, userCountry));
-    $("#crimeIndex").html(utilities.compareIndex("crime", selectedCountry, userCountry));
-    $("#pollutionIndex").html(utilities.compareIndex("pollution", selectedCountry, userCountry));
+    $("#indexTableCountry1").html(selectedCountry.admin.iso[0]);
+    $("#indexTableCountry2").html(userCountry.admin.iso[0]);
+    $("#indexTableComparison").html(`${userCountry.admin.iso[0]} compared to ${selectedCountry.admin.iso[0]}`);
 
-    $("#timezone").html(); //Copy displayLanguages function
+    $("#healthIndexCountry1").html(utilities.validIndexCheck(selectedCountry.index.healthcare));
+    $("#healthIndexCountry2").html(utilities.validIndexCheck(userCountry.index.healthcare));
+    $("#healthIndexComparison").html(utilities.compareIndex(selectedCountry.index.healthcare, userCountry.index.healthcare));
+
+    $("#crimeIndexCountry1").html(utilities.validIndexCheck(selectedCountry.index.crime));
+    $("#crimeIndexCountry2").html(utilities.validIndexCheck(userCountry.index.crime));
+    $("#crimeIndexComparison").html(utilities.compareIndex(selectedCountry.index.crime, userCountry.index.crime));
+
+    $("#pollutionIndexCountry1").html(utilities.validIndexCheck(selectedCountry.index.pollution));
+    $("#pollutionIndexCountry2").html(utilities.validIndexCheck(userCountry.index.pollution));
+    $("#pollutionIndexComparison").html(utilities.compareIndex(selectedCountry.index.pollution, userCountry.index.pollution));
+
+    $("#trafficIndexCountry1").html(utilities.validIndexCheck(selectedCountry.index.traffic));
+    $("#trafficIndexCountry2").html(utilities.validIndexCheck(userCountry.index.traffic));
+    $("#trafficIndexComparison").html(utilities.compareIndex(selectedCountry.index.traffic, userCountry.index.traffic));
+
+    displayTimezones();
     
     $("#countryModal").removeClass(" modalOff");
     
