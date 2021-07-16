e5ba6f268afc51da390188fc3f442b11c62f3569
Tyrone Dale Power <46249992+TyDPower@users.noreply.github.com>
2021-07-16 17:08:20 +0100

    Files moved to new folder

--- php/checkHTTPS.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if(!isset($_SERVER["HTTPS"]) || $_SERVER["HTTPS"] != "on")
-{
-    header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"], true, 301);
-    exit;
-}
-
-?>
