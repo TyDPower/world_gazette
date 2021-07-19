a9679af6aa903398be32d3eb8df36a950cbb0c45
Tyrone Dale Power <46249992+TyDPower@users.noreply.github.com>
2021-07-19 17:43:58 +0100

    Update getCountryInfo.php
    Removed needless api calls

--- php/getCountryInfo.php
+++ php/getCountryInfo.php
@@ -6,10 +6,6 @@
     $ch_1 = curl_init();
     $ch_2 = curl_init();
     $ch_3 = curl_init();
-    $ch_4 = curl_init();
-    $ch_5 = curl_init();
-    $ch_6 = curl_init();
-    $ch_7 = curl_init();
 
     // set URL and other appropriate options
     curl_setopt($ch_1, CURLOPT_URL, "https://restcountries.eu/rest/v2/alpha/" . $_REQUEST["code"]);
@@ -24,22 +20,6 @@
     curl_setopt($ch_3, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($ch_3, CURLOPT_RETURNTRANSFER, true);
 
-    curl_setopt($ch_4, CURLOPT_URL, "http://api.geonames.org/searchJSON?q=&country=" . $_REQUEST["code"] . "&maxRows=1000&featureCode=MT&username=tydpower");
-    curl_setopt($ch_4, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch_4, CURLOPT_RETURNTRANSFER, true);
-
-    curl_setopt($ch_5, CURLOPT_URL, "http://api.geonames.org/searchJSON?q=&country=" . $_REQUEST["code"] . "&maxRows=1000&featureCode=STDM&username=tydpower");
-    curl_setopt($ch_5, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch_5, CURLOPT_RETURNTRANSFER, true);
-
-    curl_setopt($ch_6, CURLOPT_URL, "http://api.geonames.org/searchJSON?q=&country=" . $_REQUEST["code"] . "&maxRows=1000&featureCode=CSTL&username=tydpower");
-    curl_setopt($ch_6, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch_6, CURLOPT_RETURNTRANSFER, true);
-
-    curl_setopt($ch_7, CURLOPT_URL, "http://api.geonames.org/searchJSON?q=&country=" . $_REQUEST["code"] . "&maxRows=1000&featureCode=AIRP&username=tydpower");
-    curl_setopt($ch_7, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch_7, CURLOPT_RETURNTRANSFER, true);
-
     //create the multiple cURL handle
     $mh = curl_multi_init();
 
@@ -47,10 +27,6 @@
     curl_multi_add_handle($mh,$ch_1);
     curl_multi_add_handle($mh,$ch_2);
     curl_multi_add_handle($mh,$ch_3);
-    curl_multi_add_handle($mh,$ch_4);
-    curl_multi_add_handle($mh,$ch_5);
-    curl_multi_add_handle($mh,$ch_6);
-    curl_multi_add_handle($mh,$ch_7);
 
     //execute the multi handle
     do {
@@ -64,20 +40,12 @@
     curl_multi_remove_handle($mh, $ch_1);
     curl_multi_remove_handle($mh, $ch_2);
     curl_multi_remove_handle($mh, $ch_3);
-    curl_multi_remove_handle($mh, $ch_4);
-    curl_multi_remove_handle($mh, $ch_5);
-    curl_multi_remove_handle($mh, $ch_6);
-    curl_multi_remove_handle($mh, $ch_7);
     curl_multi_close($mh);
 
     //returned data
     $restCtry = curl_multi_getcontent($ch_1);
     $news = curl_multi_getcontent($ch_2);
     $wiki = curl_multi_getcontent($ch_3);
-    $mounts = curl_multi_getcontent($ch_4);
-    $stadiums = curl_multi_getcontent($ch_5);
-    $castles = curl_multi_getcontent($ch_6);
-    $airports = curl_multi_getcontent($ch_7);
 
     $endTime = microtime(true);
     $totalTime = $endTime - $startTime;
@@ -88,10 +56,6 @@
     $output["restCtry"] = json_decode($restCtry, true);
     $output["news"] = json_decode($news, true);
     $output["wiki"] = json_decode($wiki, true);
-    $output["mounts"] = json_decode($mounts, true);
-    $output["stadiums"] = json_decode($stadiums, true);
-    $output["castles"] = json_decode($castles, true);
-    $output["airports"] = json_decode($airports, true);
 
     header("Content-Type: application/json; charset=UTF-8");
 
