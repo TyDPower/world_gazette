cf1186b46e92be8ac076c7c28f2d05874dfc43a7
Tyrone Dale Power <46249992+TyDPower@users.noreply.github.com>
2021-07-20 23:49:50 +0100

    Files moved to php/old for temp storage

---
+++ php/old/getImages.php
@@ -0,0 +1,41 @@
+<?php
+
+    ini_set("display_error", "On");
+    error_reporting(E_ALL);
+
+    $startTime = microtime(true);
+
+    $url="https://pixabay.com/api/?key=22562417-a852f90eda44cc531e1b38810&q=" . $_REQUEST["search"] . "&per_page=50";
+
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_URL, $url);
+
+    $result = curl_exec($curl);
+    $result = json_decode($result, true);
+
+    curl_close($curl);
+
+    $images = [];
+
+    foreach ($result["hits"] as $img) {
+
+        $temp = [];
+        $temp["large"] = $img["webformatURL"];
+        $temp["thumb"] = $img["previewURL"];
+
+        array_push($images, $temp);
+
+    }
+
+    $output["status"]["code"] = "200";
+    $output["status"]["name"] = "ok";
+    $output["status"]["description"] = "success";
+    $output["data"] = $images;
+
+    header("Content-Type: application/json; charset=UTF-8");
+
+    echo json_encode($output);
+
+?>
